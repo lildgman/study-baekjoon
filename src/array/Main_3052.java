0ab0b1b1197d2bf00f1282f6864e96951c226a03
lildgman <humanorc2357@gmail.com>
2024-02-15 17:49:47 +0900

    Add 백준 배열

---
+++ src/array/Main_3052.java
@@ -0,0 +1,23 @@
+package array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
+            set.add(num % 42);
+        }
+        System.out.println(set.size());
+
+
+
+    }
+}
