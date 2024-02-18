bc3859e224546b993382f63f693c50e3555dfc8a
lildgman <humanorc2357@gmail.com>
2024-02-18 20:09:07 +0900

    1157번 푸는 중

---
+++ src/deepen1/Main_2444.java
@@ -0,0 +1,34 @@
+package deepen1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2444 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < i * 2 + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n-1; i > 0; i--) {
+            for (int j = n-1; j > i - 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
