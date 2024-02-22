4d3753c5a325bf49f3f50d33aa1f543df701561c
lildgman <humanorc2357@gmail.com>
2024-02-22 16:56:55 +0900

    2292까지 풀었음

---
+++ src/math/Main_2903.java
@@ -0,0 +1,23 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2903 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        int start = 2;
+
+        for (int i = num - 1; i >= 0; i--) {
+            start += (int) Math.pow(2, i);
+        }
+        System.out.println(start*start);
+
+    }
+
+}
+
